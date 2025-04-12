270e7fba8b19a6d94e830e78e023cd3c11f8b537
Sujan Reddy <sujanreddy242@gmail.com>
2025-04-12 18:22:55 -0500

    made embeddings on GPU

--- utils/embedding_model.py
+++ utils/embedding_model.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
 from sentence_transformers import SentenceTransformer
 from typing import List
+import torch
 
 class EmbeddingModel:
 
@@ -19,8 +20,13 @@ class EmbeddingModel:
 
 class SentenceTransformerEmbeddingModel(EmbeddingModel):
     def __init__(self, model_name: str):
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model = SentenceTransformer(model_name)
 
     def get_embedding(self, sentences: List[str]):
         # TODO split across GPUs
-        return self.model.encode(sentences, convert_to_tensor=True, batch_size=len(sentences))
+        return self.model.encode(sentences,
+                                 convert_to_tensor=True,
+                                 batch_size=len(sentences),
+                                 device=self.device
+                                 )
