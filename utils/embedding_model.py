8e8d6391d1c3a4f3a713b828fdd531fe96c5490b
Sujan Reddy <sujanreddy242@gmail.com>
2025-04-12 19:07:45 -0500

    run blocking on single GPU

--- utils/embedding_model.py
+++ utils/embedding_model.py
@@ -20,8 +20,15 @@ class EmbeddingModel:
 
 class SentenceTransformerEmbeddingModel(EmbeddingModel):
     def __init__(self, model_name: str):
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.model = SentenceTransformer(model_name)
+        if torch.cuda.is_available():
+            print(f"Using GPU for embedding: {torch.cuda.get_device_name(0)}")
+            self.device = 'cuda'
+            print("Using device: ", self.device)
+        else:
+            print("Using CPU for embedding")
+            self.device = 'cpu'
+
+        self.model = SentenceTransformer(model_name, device=self.device)
 
     def get_embedding(self, sentences: List[str]):
         # TODO split across GPUs
--- utils/index.py
+++ utils/index.py
@@ -1,4 +1,12 @@
 import faiss
+import torch
+
 
 def get_index(dim):
+    if torch.cuda.is_available():
+        print("Using GPU for FAISS index")
+        res = faiss.StandardGpuResources()
+        return faiss.GpuIndexFlatL2(res, dim)
+
+    print("Using CPU for FAISS index")
     return faiss.IndexFlatL2(dim)
