1151e44d683b510a78ef5c1291ee53d73793e87b
lparab <loukikjp@gmail.com>
2025-05-04 15:34:27 -0500

    Fixed error

--- amazon-google.py
+++ amazon-google.py
@@ -134,11 +134,10 @@ if __name__ == "__main__":
     perfect_mapping_path = os.path.join(data_path, "walmart_amazon_perfectmapping.csv")
     perfect_mapping_df = pd.read_csv(perfect_mapping_path)
     ground_truth = {} # dict(zip(perfect_mapping_df['id1'],perfect_mapping_df['id2']))
+    srcLabel, tgtLabel = 'id1', 'id2'
     for rNum, row in perfect_mapping_df.iterrows():
-        srcLabel = 'id1'
-        tgtLabel = 'id2'
         if row[srcLabel] in ground_truth.keys():
-            row[srcLabel].append(row[tgtLabel])
+            ground_truth[row[srcLabel]].append(row[tgtLabel])
         else:
             ground_truth[row[srcLabel]] = [row[tgtLabel]]
 
